87c5a4bcb20a59e7aeb7a1b86f939dbbc37d5415
Wyatt McKenna <pbtwlm@yahoo.com>
2024-11-14 09:09:41 -1000

    Code example

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom'
+import Homepage from './pages/Homepage/Homepage';
+import Login from './pages/Login/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path ="/login" element={<Login/>} />
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default App;
---
+++ src/components/GeneralButton/GeneralButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './GeneralButton.module.css'
+
+function GeneralButton({ text, onClick, disabled = false }) {
+    return (
+    <button
+         onClick={onClick}
+        className={styles.generalButton}
+        disabled={disabled}
+    >
+
+        {text} 
+    </button>
+    )
+}
+
+export default GeneralButton;
